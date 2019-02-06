ef3b110986b6d046564eaacf9a9bc6410fb452f4
SkeletonHoney <ryanerogers5@gmail.com>
2019-02-05 20:00:28 -0500

    Added Holdem
    New sln file added in folder Holdem. Took an AI off of github and added/changed some methods. It should be good to go.

---
+++ Holdem/Holdem/Card.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Resources;
+namespace Holdem
+{
+    public enum RANK
+    {
+        TWO=2, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+    }
+    public enum SUIT
+    {
+        HEARTS = 1,
+        CLUBS,
+        DIAMONDS,
+        SPADES
+    }
+    /// <summary>
+    /// THIS IS THE CLASS THAT STARTED EVERYTHING AND MADE THIS GAME POSSIBLE
+    /// </summary>
+    public class Card
+    {
+        private int rank, suit;
+        private char mark; //what state the card is marked in.
+        private Bitmap Image;
+        private bool faceUp; //whether or not the card is actually face up
+        private bool highlight;
+        public bool FaceUp
+        {
+            get { return faceUp; }
+            set 
+            { 
+                faceUp = value;
+                getImageFromFile();
+            }
+        }
+        //default two of diamonds
+        public Card()
+        {
+            rank = (int)RANK.TWO;
+            suit = (int)SUIT.DIAMONDS;
+            this.mark = 'N';
+            faceUp = false;
+            highlight = false;
+        }
+        public Card(RANK rank,SUIT suit)
+        {
+            this.rank = (int)rank;
+            this.suit = (int)suit;
+            this.mark = 'N';
+            faceUp = false;
+            highlight = false;
+        }
+        public Card(int rank, int suit)
+        {
+            if (rank < 1 || rank > 14 || suit < 1 || suit > 4)
+                throw new ArgumentOutOfRangeException();
+            this.rank=rank;
+            this.suit=suit;
+            this.mark = 'N';
+            faceUp =false;
+            highlight = false;
+        }
+        public Card(RANK rank, SUIT suit,bool faceUp)
+        {
+            this.rank = (int)rank;
+            this.suit = (int)suit;
+            this.mark = 'N';
+            this.faceUp = faceUp;
+            highlight = false;
+        }
+        public Card(int rank, int suit,bool faceUp)
+        {
+            if (rank < 1 || rank > 14 || suit < 1 || suit > 4)
+                throw new ArgumentOutOfRangeException();
+            this.rank = rank;
+            this.suit = suit;
+            this.mark = 'N';
+            this.faceUp = faceUp;
+            highlight = false;
+        }
+        public Card(RANK rank, SUIT suit, char m)
+        {
+            this.rank = (int)rank;
+            this.suit = (int)suit;
+            setMark(m);
+            faceUp = false;
+            highlight = false;
+        }
+        public Card(int rank, int suit, char m)
+        {
+            if (rank < 1 || rank > 14 || suit < 1 || suit > 4)
+                throw new ArgumentOutOfRangeException();
+            this.rank = rank;
+            this.suit = suit;
+            setMark(m);
+            faceUp = false;
+            highlight = false;
+        }
+        public Card(RANK rank, SUIT suit, char m, bool faceUp)
+        {
+            this.rank = (int)rank;
+            this.suit = (int)suit;
+            setMark(m);
+            this.faceUp = faceUp;
+            highlight = false;
+        }
+        public Card(int rank, int suit, char m, bool faceUp)
+        {
+            if (rank < 1 || rank > 14 || suit < 1 || suit > 4)
+                throw new ArgumentOutOfRangeException();
+            this.rank = rank;
+            this.suit = suit;
+            setMark(m);
+            this.faceUp = faceUp;
+            highlight = false;
+        }
+        public Card(Card card)
+        {
+            this.rank = card.rank;
+            this.suit = card.suit;
+            this.mark = card.mark;
+            this.faceUp = card.faceUp;
+            highlight = false;
+        }
+        public static string rankToString(int rank)
+        {
+            switch (rank)
+            {
+                case 11:
+                    return "Jack";
+                case 12:
+                    return "Queen";
+                case 13:
+                    return "King";
+                case 14:
+                    return "Ace";
+                default:
+                    return rank.ToString();
+            }
+        }
+        public static string suitToString(int suit)
+        {
+            switch (suit)
+            {
+                case 1:
+                    return "Hearts";
+                case 2:
+                    return "Clubs";
+                case 3:
+                    return "Diamonds";
+                default:
+                    return "Spades";
+            }
+        }
+        public static char suitToChar(int suit)
+        {
+            switch (suit)
+            {
+                case 1:
+                    return 'H';
+                case 2:
+                    return 'C';
+                case 3:
+                    return 'D';
+                default:
+                    return 'S';
+            }
+        }
+        public int getRank()
+        {
+            return rank;
+        }
+        public int getSuit()
+        {
+            return suit;
+        }
+        //get image from depending on if the card is faceup or down
+        private void getImageFromFile()
+        {
+            if (faceUp)
+                this.Image = new Bitmap("Cards\\" + suit + "-" + rank + ".png");
+            else
+                this.Image = new Bitmap("Cards\\sb.bmp");
+        }
+        //get the current image
+        public Bitmap getImage()
+        {
+            if(Image==null)
+                getImageFromFile();
+            return this.Image;
+        }
+        public void setRank(RANK rank)
+        {
+            this.rank = (int)rank;
+        }
+        public bool isMarked()
+        {
+            return (mark != 'N');
+        }
+        public void setMark(char m)
+        {
+            if(m == 'T' || m == 'P')
+            {
+                this.mark = m;
+            }
+            else
+            {
+                this.mark = 'N';
+            }
+        }
+        public char getMark()
+        {
+            return mark;
+        }
+        public void setCard(RANK rank, SUIT suit)
+        {
+            this.rank = (int)rank;
+            this.suit = (int)suit;
+        }
+        public void setCard(int rank, int suit)
+        {
+            if(rank<1||rank>14||suit<1||suit>4)
+                throw new ArgumentOutOfRangeException();
+            this.rank=rank;
+            this.suit=suit;
+        }
+        public override string ToString()
+        {
+            if (faceUp == true)
+                return rankToString(rank) + " of " + suitToString(suit);
+            return "The card is facedown, you cannot see it!";
+        }
+        
+        //extract green channel from image to highlight image
+        public void Highlight()
+        {
+            if (faceUp == false)
+                return;
+
+            this.highlight = true;
+
+            if (this.Image == null)
+                getImageFromFile();
+            Bitmap HighlightedBitmap = new Bitmap(Image.Width, Image.Height);
+            for (int i = 0; i < Image.Width; i++)
+            {
+                for (int j = 0; j < Image.Height; j++)
+                {
+                    int green = Image.GetPixel(i, j).G;
+                    HighlightedBitmap.SetPixel(i, j, Color.FromArgb(255, 0, green, 0));
+                }
+            }
+            Image = new Bitmap(HighlightedBitmap);
+        }
+        //reload original image to unhighlight
+        public void UnHighlight()
+        {
+            if (faceUp == false)
+                return;
+            this.highlight = false;
+            getImageFromFile();
+        }
+        public bool isHighlighted()
+        {
+            return this.highlight;
+        }
+
+        //compare rank of cards
+        public static bool operator ==(Card a, Card b)
+        {
+            if (a.rank == b.rank)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(Card a, Card b)
+        {
+            if (a.rank != b.rank)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator <(Card a, Card b)
+        {
+            if (a.rank < b.rank)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator >(Card a, Card b)
+        {
+            if (a.rank > b.rank)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator <=(Card a, Card b)
+        {
+            if (a.rank <= b.rank)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator >=(Card a, Card b)
+        {
+            if (a.rank >= b.rank)
+                return true;
+            else
+                return false;
+        }
+    }
+    
+}
---
+++ Holdem/Holdem/Deck.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Holdem
+{
+    /// <summary>
+    /// standard fair deck of 52 cards
+    /// </summary>
+    public class Deck
+    {
+        private List<Card> deck = new List<Card>();
+        public Deck()
+        {
+            deck.Add(new Card(14, 1));
+            for (byte i = 2; i <= 13; i++)
+            {
+                deck.Add(new Card(i, 1));
+            }
+            deck.Add(new Card(14, 2));
+            for (byte i = 2; i <= 13; i++)
+            {
+                deck.Add(new Card(i, 2));
+            }
+            for (byte i = 13; i >= 2; i--)
+            {
+                deck.Add(new Card(i, 3));
+            }
+            deck.Add(new Card(14, 3));
+            for (byte i = 13; i >= 2; i--)
+            {
+                deck.Add(new Card(i, 4));
+            }
+            deck.Add(new Card(14, 4));
+        }
+        public Deck(bool faceUp)
+        {
+            deck.Add(new Card(14, 1, faceUp));
+            for (byte i = 2; i <= 13; i++)
+            {
+                deck.Add(new Card(i, 1, faceUp));
+            }
+            deck.Add(new Card(14, 2, faceUp));
+            for (byte i = 2; i <= 13; i++)
+            {
+                deck.Add(new Card(i, 2, faceUp));
+            }
+            for (byte i = 13; i >= 2; i--)
+            {
+                deck.Add(new Card(i, 3, faceUp));
+            }
+            deck.Add(new Card(14, 3, faceUp));
+            for (byte i = 13; i >= 2; i--)
+            {
+                deck.Add(new Card(i, 4, faceUp));
+            }
+            deck.Add(new Card(14, 4, faceUp));
+        }
+        public Deck(Deck otherDeck)
+        {
+            foreach (Card card in otherDeck.deck)
+            {
+                this.deck.Add(new Card(card));
+            }
+        }
+        public void Add(Card card)
+        {
+            deck.Add(card);
+        }
+        public int CardsLeft()
+        {
+            return deck.Count;
+        }
+        //<SHUFFLING>
+        public void Shuffle()
+        {
+            var rand = new Random();
+            for (int i = CardsLeft() - 1; i > 0; i--)
+            {
+                int n = rand.Next(i + 1);
+                Card temp = deck[i];
+                deck[i] = deck[n];
+                deck[n] = temp;
+            }
+        }
+        public void PerfectCut()
+        {
+            Card c;
+            int x = (int)Math.Floor((double)deck.Count / 2.0);
+            for (int i = 1; i <= x; i++)
+            {
+                c = deck[deck.Count - 1];
+                deck.RemoveAt(deck.Count - 1);
+                deck.Insert(0, c);
+            }
+        }
+        //human cut
+        public void Cut()
+        {
+            Card c;
+            int x = 0;
+            Random r = new Random();
+            for (int i = 1; i <= deck.Count; i++) { x += r.Next(2); }
+            for (int i = 1; i <= x; i++)
+            {
+                c = deck[deck.Count - 1];
+                deck.RemoveAt(deck.Count - 1);
+                deck.Insert(0, c);
+            }
+        }
+        //perfect riffle
+        public void PerfectRiffle(bool preserveEnds)
+        {
+            List<Card> temp1 = new List<Card>();
+            List<Card> temp2 = new List<Card>();
+            int x = (int)Math.Floor((double)deck.Count / 2.0);
+            int size = deck.Count();
+            for (int i = 1; i <= x; i++)
+            {
+                temp1.Insert(0, deck[deck.Count - 1]);
+                deck.RemoveAt(deck.Count - 1);
+            }
+            for (int i = 1; i <= x; i++)
+            {
+                temp2.Insert(0, deck[deck.Count - 1]);
+                deck.RemoveAt(deck.Count - 1);
+            }
+            for (int i = 1; i <= size; i++)
+            {
+                if (preserveEnds)
+                {
+                    deck.Insert(0, temp1[temp1.Count() - 1]);
+                    temp1.RemoveAt(temp1.Count() - 1);
+                }
+                else
+                {
+                    deck.Insert(0, temp2[temp2.Count() - 1]);
+                    temp2.RemoveAt(temp2.Count() - 1);
+                }
+                preserveEnds = !preserveEnds;
+            }
+        }
+        //random riffle
+        public void Riffle(bool perfectCut)
+        {
+            //resources
+            List<Card> temp1 = new List<Card>();
+            List<Card> temp2 = new List<Card>();
+            int x, size = deck.Count;
+            Random r = new Random();
+
+            //finds where to split
+            if (perfectCut)
+            {
+                x = (int)Math.Floor((double)deck.Count / 2.0);
+            }
+            else
+            {
+                x = 0;
+                for (int i = 1; i <= deck.Count; i++) { x += r.Next(2); }
+            }
+
+            //splits deck
+            for (int i = size - 1; i >= x; i--)
+            {
+                temp1.Insert(0, deck[i]);
+                deck.RemoveAt(i);
+            }
+            for (int i = x - 1; i >= 0; i--)
+            {
+                temp2.Insert(0, deck[i]);
+                deck.RemoveAt(i);
+            }
+
+            //riffle
+            for (int i = 1; i <= size; i++)
+            {
+                if (r.Next(2) == 0)
+                {
+                    if (temp1.Count() == 0)
+                    {
+                        goto finish2;
+                    }
+                    deck.Insert(0, temp1[temp1.Count() - 1]);
+                    temp1.RemoveAt(temp1.Count() - 1);
+                }
+                else
+                {
+                    if (temp2.Count() == 0)
+                    {
+                        goto finish1;
+                    }
+                    deck.Insert(0, temp2[temp2.Count() - 1]);
+                    temp2.RemoveAt(temp2.Count() - 1);
+                }
+            }
+
+            //if imperfect split
+            finish1:
+            for (int i = temp1.Count() - 1; i >= 0; i--)
+            {
+                deck.Insert(0, temp1[i]);
+                temp1.RemoveAt(i);
+            }
+            return;
+            finish2:
+            for (int i = temp2.Count() - 1; i >= 0; i--)
+            {
+                deck.Insert(0, temp2[i]);
+                temp2.RemoveAt(i);
+            }
+            return;
+        }
+        public void Strip()
+        {
+            //resources
+            List<Card> temp = new List<Card>();
+            Random r = new Random();
+            int x;
+
+            //strip all cards
+            while (deck.Count() > 0)
+            {
+                //select random number
+                x = 11; //offset
+                for (int i = 0; i < 14; i++) { x += r.Next(2); }
+                if (x > deck.Count() - 1) { x = deck.Count() - 1; } //prevent overflow
+
+                //create strip and put in temp
+                for (int i = x; i >= 0; i--)
+                {
+                    temp.Insert(0, deck[i]);
+                    deck.RemoveAt(i);
+                }
+            }
+
+            //put temp into deck
+            while (temp.Count() != 0)
+            {
+                deck.Add(temp[0]);
+                temp.RemoveAt(0);
+            }
+            return;
+        }
+        //</SHUFFLING>
+        //<MARKING>
+        public void Mark(Card c, char mark)
+        {
+            deck.Find(x => x.Equals(c)).setMark(mark);
+        }
+        public void Mark(byte rank, byte suit, char mark)
+        {
+            deck.Find(x => x.Equals(new Card(rank, suit))).setMark(mark);
+        }
+        public void Mark(RANK rank, SUIT suit, char mark)
+        {
+            deck.Find(x => x.Equals(new Card(rank, suit))).setMark(mark);
+        }
+        //</MARKING>
+        //<PRINTING>
+        public string Print()
+        {
+            string output = "";
+            foreach (Card card in deck)
+            {
+                output += card.ToString() + " ";
+            }
+            return output;
+        }
+        //</PRINTING>
+        //<DEALING CARDS>
+        public Card Deal()
+        {
+            Card c = deck[0];
+            deck.RemoveAt(0);
+            c.FaceUp = true;
+            return c;
+        }
+        public Card Deal(bool faceUp)
+        {
+            Card c = deck[0];
+            deck.RemoveAt(0);
+            c.FaceUp = faceUp;
+            return c;
+        }
+        public Card SecondDeal()
+        {
+            Card c = deck[1];
+            deck.RemoveAt(1);
+            c.FaceUp = true;
+            return c;
+        }
+        public Card SecondDeal(bool faceUp)
+        {
+            Card c = deck[1];
+            deck.RemoveAt(1);
+            c.FaceUp = faceUp;
+            return c;
+        }
+        public Card BottomDeal()
+        {
+            Card c = deck[deck.Count - 1];
+            deck.RemoveAt(deck.Count - 1);
+            c.FaceUp = true;
+            return c;
+        }
+        public Card BottomDeal(bool faceUp)
+        {
+            Card c = deck[deck.Count - 1];
+            deck.RemoveAt(deck.Count - 1);
+            c.FaceUp = faceUp;
+            return c;
+        }
+        //</DEALING CARDS>
+
+        public void Remove(int index)
+        {
+            if (index < 0 || index >= deck.Count)
+                throw new ArgumentOutOfRangeException();
+            deck.RemoveAt(index);
+        }
+        public void Remove(Card card)
+        {
+            for(int i=0;i<deck.Count;i++)
+            {
+                if (deck[i] == card && deck[i].getSuit() == card.getSuit())
+                {
+                    deck.RemoveAt(i);
+                }
+            }
+        }
+        public Card[] ToArray()
+        {
+            return deck.ToArray();
+        }
+        public List<Card> ToList()
+        {
+            return deck;
+        }
+    }
+}
